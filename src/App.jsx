7e4841f09bf6006b0906aa4d9227f8c7d02c8d69
Mohammed Amine Rafi <mohammed.amine.rafi2006@gmail.com>
2025-12-14 23:14:26 +0100

    Fix setShowLoginForm prop passing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/home";
-import Catalogue from "./pages/catalogue";
-import CourseAdd from "./pages/courseAdd";
-import CourseEdit from "./pages/courseEdit";
-import CoursePage from "./pages/coursePage";
-import Dashboard from "./pages/dashboard";
-import AboutUs from "./pages/aboutUs";
-
-import Navbar from "./components/navbar";
-import { useState } from "react";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Catalogue from './pages/catalogue';
+import CourseAdd from './pages/courseAdd';
+import CourseEdit from './pages/courseEdit';
+import CoursePage from './pages/coursePage';
+import Dashboard from './pages/dashboard';
+import AboutUs from './pages/aboutUs';
+import Navbar from './components/navbar';
+import { useState } from 'react';
 
 function App() {
   const [showLoginForm, setShowLoginForm] = useState(false);
@@ -18,21 +16,21 @@ function App() {
     <Router>
       <div className="grid grid-cols-12 gap-3 p-3 h-screen bg-neutral-100">
         <div className="col-span-2 h-full">
-          <Navbar
-            showLoginForm={showLoginForm}
-            setShowLoginForm={setShowLoginForm}
+          <Navbar 
+            showLoginForm={showLoginForm} 
+            setShowLoginForm={setShowLoginForm} 
           />
         </div>
         <div className="col-span-10 bg-white rounded-lg overflow-y-auto h-full">
           <Routes>
-            <Route
-              path="/Skill-Hub"
+            <Route 
+              path="/" 
               element={
-                <Home
-                  showLoginForm={showLoginForm}
-                  setShowLoginForm={setShowLoginForm}
+                <Home 
+                  showLoginForm={showLoginForm} 
+                  setShowLoginForm={setShowLoginForm} 
                 />
-              }
+              } 
             />
             <Route path="/catalogue" element={<Catalogue />} />
             <Route path="/course/add" element={<CourseAdd />} />
@@ -40,7 +38,6 @@ function App() {
             <Route path="/course/:id" element={<CoursePage />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/about" element={<AboutUs />} />
-            <Route path="*" element={<Home />} />
           </Routes>
         </div>
       </div>
